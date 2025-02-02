7ac392c1a77a84b00382acf2313a053a238b9717
samrat-dey-ces <samrat.dey@cesltd.com>
2025-02-03 02:26:32 +0530

    aaa

--- scripts/setup-hooks.js
+++ scripts/setup-hooks.js
@@ -11,11 +11,14 @@ execSync('npx husky', { stdio: 'inherit' });
 // Path to the pre-commit hook file
 const preCommitHookPath = path.resolve('.husky', 'pre-commit');
 const preCommitJSPath = path.resolve('scripts', 'pre-commit.js');
+const commitMsgPath = path.resolve('.husky', 'commit-msg');
 
 // Create or overwrite the pre-commit hook file with the desired script
-const preCommitScript = `# Run lint-staged to lint and format code before commit
-node ./scripts/pre-commit.mjs
-`;
+const preCommitScript = `node ./scripts/pre-commit.mjs`;
+
+// Create or overwrite the commit-msg hook file with the desired script
+const commitMsgScript = `npx --no-install commitlint --edit $1`;
+
 
 // Ensure .husky directory exists
 if (!fs.existsSync(path.resolve('.husky'))) {
@@ -25,6 +28,8 @@ if (!fs.existsSync(path.resolve('.husky'))) {
 
 // Write the pre-commit hook script
 fs.writeFileSync(preCommitHookPath, preCommitScript, 'utf8');
+// Write the commit-msg hook script
+fs.writeFileSync(commitMsgPath, commitMsgScript, 'utf8');
 
 // Check if we're on a Unix-like system (macOS/Linux) and only run chmod there
 if (process.platform !== 'win32') {
@@ -32,6 +37,7 @@ if (process.platform !== 'win32') {
     console.log('✅ Setting execute permissions on pre-commit hook...');
     execSync(`chmod +x ${preCommitHookPath}`, { stdio: 'inherit' });
     execSync(`chmod +x ${preCommitJSPath}`, { stdio: 'inherit' });
+    execSync(`chmod +x ${commitMsgPath}`, { stdio: 'inherit' });
   } catch (err) {
     console.error('❌ Error setting execute permissions in your Unix-like system.');
     process.exit(1);
@@ -39,3 +45,4 @@ if (process.platform !== 'win32') {
 }
 
 console.log('✅ Husky pre-commit hook setup with lint-staged completed!');
+console.log('✅ Husky commit-msg hook setup with lint-staged completed!');
