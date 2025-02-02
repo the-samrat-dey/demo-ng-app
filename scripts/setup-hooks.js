1d05e728e5f65a669eadc1b4c61a3d827f2d34dd
samrat-dey-ces <samrat.dey@cesltd.com>
2025-02-02 23:13:33 +0530

    initial commit

---
+++ scripts/setup-hooks.js
@@ -0,0 +1,45 @@
+const { execSync } = require('child_process');
+const fs = require('fs');
+const path = require('path');
+
+// Run husky init to set up Git hooks
+execSync('ng analytics off', { stdio: 'inherit' });
+
+// Run husky init to set up Git hooks
+execSync('npx husky', { stdio: 'inherit' });
+
+// Path to the pre-commit hook file
+const preCommitHookPath = path.resolve('.husky', 'pre-commit');
+
+// Create or overwrite the pre-commit hook file with the desired script
+const preCommitScript = `#!/bin/sh
+. "$(dirname "$0")/_/husky.sh"
+
+# Run lint-staged to lint and format code before commit
+npx --no-install lint-staged || {
+  echo "\x1b[1;31m❌ Commit rejected! Linting or formatting failed. Please fix the errors and try again.\x1b[0m"
+  exit 1
+}
+`;
+
+// Ensure .husky directory exists
+if (!fs.existsSync(path.resolve('.husky'))) {
+  console.error('❌ .husky directory not found! Please create the .husky directory first.');
+  process.exit(1); // Exit the script with an error code
+}
+
+// Write the pre-commit hook script
+fs.writeFileSync(preCommitHookPath, preCommitScript, 'utf8');
+
+// Check if we're on a Unix-like system (macOS/Linux) and only run chmod there
+if (process.platform !== 'win32') {
+  try {
+    console.log('✅ Setting execute permissions on pre-commit hook...');
+    execSync(`chmod +x ${preCommitHookPath}`, { stdio: 'inherit' });
+  } catch (err) {
+    console.error('❌ Error setting execute permissions in your Unix-like system.');
+    process.exit(1);
+  }
+}
+
+console.log('✅ Husky pre-commit hook setup with lint-staged completed!');
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'demo-ng-app';
+  title =
+    'demo-ng-app sdf   sd fd sf ds f ds fd  f df sd f sd f d f dsf ds f sd f ds f sdf  sdf ds f ds f ds f ds f dsf ds f ds f sdf dsf d';
 }
